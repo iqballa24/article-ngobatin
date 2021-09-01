af1ac526f3df4aa3aeed67b996762be9dc3a7666
iqballa24 <iqbalnugraha347@gmail.com>
2021-09-01 08:28:21 +0700

    update path link navbar

--- application/views/theme/user/navbar.php
+++ application/views/theme/user/navbar.php
@@ -4,10 +4,10 @@
 <nav class="navbar">
     <ul class="nav__links my-auto">
         <?php $i = $this->uri->segment(1) ?>
-        <li><a href="http://localhost/ngobatin/home/" class="<?php if ($i == 'home') { echo "active";} ?>">Beranda</a></li>
-        <li><a href="http://localhost/ngobatin/about/read" class="<?php if ($i == 'about') { echo "active";} ?>">Tentang</a></li>
-        <li><a href="http://localhost/ngobatin/article/read" class="<?php if ($i == 'article') { echo "active";} ?>" >Artikel</a></li>
-        <li><a href="http://localhost/ngobatin/forum/read" class="<?php if ($i == 'forum') { echo "active";} ?>">Forum</a></li>
+        <li><a href="<?= base_url('home/read'); ?>" class="<?php if ($i == 'home') { echo "active";} ?>">Beranda</a></li>
+        <li><a href="<?= base_url('about/read'); ?>" class="<?php if ($i == 'about') { echo "active";} ?>">Tentang</a></li>
+        <li><a href="<?= base_url('article/read'); ?>" class="<?php if ($i == 'article') { echo "active";} ?>" >Artikel</a></li>
+        <li><a href="<?= base_url('forum/read'); ?>" class="<?php if ($i == 'forum') { echo "active";} ?>">Forum</a></li>
         <!-- <li><a class="button button__primary" href="detail.html"><i class="fas fa-hand-holding-usd"></i></a></li> -->
     </ul>
     <div class="hamburger">
